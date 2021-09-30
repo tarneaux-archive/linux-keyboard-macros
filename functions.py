80ac31f19ec6e145ab58fbc4f681e4997c4dac36
contact.tarneo@gmail.com <tarneaux>
2021-09-30 20:07:24 +0200

    use good keyboard controller

--- functions.py
+++ functions.py
@@ -1,12 +1,14 @@
-from os import system
+from pynput.keyboard import Controller
+
+keyboard = Controller()
 
 def write(text):
-    system("xdotool type \"" + text + "\"")
+    keyboard.type(text)
 
 
 # In this file, you should have a run option to be run each time the user will press a button. The argument is the int of the keycode.
 def run(code):
     if code == 458762:
-        write("linux-keyboard-macros github page: https://github.com/tarneaux/linux-keyboard-macros")
+        write("linux-keyboard-macros github page: https://github.com/tarneaux/linux-keyboard-macros\n")
     else:
         print(code)
